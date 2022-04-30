7e007a11945e0e0cd86186459b1ae4f21e0e43d9
PedroParraPerez <pedroparra06091994@gmail.com>
2022-04-30 01:11:56 +0000

    2 people per group logic and css finish, next step 3 people per group

---
+++ src/front/js/component/cardfor2person.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import "../../styles/card.css";
+
+export const Cardfor2person = (props) => {
+
+
+  return (
+    <>
+      
+    </>
+  );
+};
+Card.propTypes = {
+    group: PropTypes.string,
+    student1: PropTypes.string,
+    student2: PropTypes.string
+}
