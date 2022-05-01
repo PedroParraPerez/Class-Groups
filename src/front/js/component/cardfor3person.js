8deda51d04af73afacf2a5e9ec54e868937e4607
PedroParraPerez <pedroparra06091994@gmail.com>
2022-05-01 12:08:04 +0000

    fix bugs

--- src/front/js/component/cardfor3person.js
+++ src/front/js/component/cardfor3person.js
@@ -53,7 +53,7 @@ export const Cardfor3person = () => {
   let randomNumberForPersonWithoutGroup = []
   const generateTwoRandomNumbersDifferentForPersonWithoutGroup = () => {
     while (randomNumberForPersonWithoutGroup.length < 2) {
-      let r = Math.floor(Math.random() * store.STUDENTS.length/3);
+      let r = Math.floor(Math.random() * store.STUDENTS.length/4) +1;
       randomNumberForPersonWithoutGroup.indexOf(r) === -1 ? randomNumberForPersonWithoutGroup.push(r) : "";
       console.log(randomNumberForPersonWithoutGroup)
     }
