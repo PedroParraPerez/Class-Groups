63498a0f1a9b5f137f3cbe301a9051d7a099b443
Pedro Parra <91670052+PedroParraPerez@users.noreply.github.com>
2022-05-17 02:57:40 +0200

    Update modalForSignUp.js

--- src/front/js/component/Modals/modalForSignUp.js
+++ src/front/js/component/Modals/modalForSignUp.js
@@ -3,10 +3,9 @@ import PropTypes from "prop-types";
 import "../../../styles/modalForSignUp.css";
 import { Context } from "../../store/appContext";
 
-
 export const ModalForSignUp = (props) => {
   const { store, actions } = useContext(Context);
-  const [register, setRegister] = useState({})
+  const [register, setRegister] = useState({});
 
   return (
     <article className={`modal ${props.stateModal}`}>
@@ -15,62 +14,91 @@ export const ModalForSignUp = (props) => {
           onClick={() => {
             actions.modalSingUp();
           }}
-          className="modal-close"
-        >
-          X
-        </button>
+          className=" modalclose btn-close  p-2 "
+        ></button>
         <h4>Registrarse</h4>
-        <input 
-        onChange={(event) => {
-            setRegister({
-              ...register,
-              [event.target.name]: event.target.value,
-            });
-          }}
-        type="text" name="email" placeholder="Email" />
-        <input
-        onChange={(event) => {
-            setRegister({
-              ...register,
-              [event.target.name]: event.target.value,
-            });
-          }}
-        type="password" name="password" placeholder="Contraseña" />
-        <input
-        onChange={(event) => {
-            setRegister({
-              ...register,
-              [event.target.name]: event.target.value,
-            });
-          }}
-        type="password" name="repeatpassword" placeholder="Repetir contraseña" />
-        <select name="type" 
-        onChange={(event) => {
-            setRegister({
-              ...register,
-              [event.target.name]: event.target.value,
-            });
-          }}
-         >
-          <option selected disabled hidden>Tipo de profesor</option>
-          <option value="teacher">Profesor Principal</option>
-          <option value="teacher assistant">
-            Profesor Asistente
-          </option>
-          
-        </select>
-
+        <div className="row mt-3">
+          <div className="col-12">
+            <input
+              onChange={(event) => {
+                setRegister({
+                  ...register,
+                  [event.target.name]: event.target.value,
+                });
+              }}
+              type="text"
+              name="email"
+              className="w-75"
+              placeholder="Email"
+            />
+          </div>
+        </div>
+        <div className="row mt-2">
+          <div className="col-12">
+            <input
+              className="w-75"
+              onChange={(event) => {
+                setRegister({
+                  ...register,
+                  [event.target.name]: event.target.value,
+                });
+              }}
+              type="password"
+              name="password"
+              placeholder="Contraseña"
+            />
+          </div>
+        </div>
+        <div className="row mt-2">
+          <div className="col-12">
+            <input
+              onChange={(event) => {
+                setRegister({
+                  ...register,
+                  [event.target.name]: event.target.value,
+                });
+              }}
+              type="password"
+              className="w-75"
+              name="repeatpassword"
+              placeholder="Repetir contraseña"
+            />
+          </div>
+        </div>
+        <div className="row mt-2">
+          <div className="col-12">
+            <select
+              name="type"
+              onChange={(event) => {
+                setRegister({
+                  ...register,
+                  [event.target.name]: event.target.value,
+                });
+              }}
+            >
+              <option selected disabled hidden>
+                Tipo de profesor
+              </option>
+              <option value="teacher">Profesor Principal</option>
+              <option value="teacher assistant">Profesor Asistente</option>
+            </select>
+          </div>
+        </div>
+        
+          <div className="d-flex justify-content-end ">
         <button
-        onClick={() => {
-            if (
-                register.password ==
-                register.repeatpassword
-            ) {
-            actions.registerTeacher(register);
-           
-              
-            }else{alert("contraseñas no coinciden")}
-          }}>Registrarse</button>
+          onClick={() => {
+            if (register.password == register.repeatpassword) {
+              actions.registerTeacher(register);
+            } else {
+              alert("contraseñas no coinciden");
+            }
+          }}
+        >
+          Registrarse
+        </button>
+      </div>
+      
       </div>
     </article>
   );
